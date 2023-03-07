200b945be5ecb6a672f6e5af129af66c3e2fafd1
tqliu <tliu@handext.com>
2023-03-06 22:29:37 -0800

    Remove /Articles API

--- api_gateway.tf
+++ api_gateway.tf
@@ -41,18 +41,6 @@ module "category_api" {
   request_validator_id = "${aws_api_gateway_request_validator.core_api_request_validator.id}"
 }
 
-module "article_api" {
-  source  = "./api/article"
-  
-  api_id               = "${aws_api_gateway_rest_api.site_core_api_gateway.id}"
-  api_execution_arn    = "${aws_api_gateway_rest_api.site_core_api_gateway.execution_arn}"
-  root_resource_id     = "${aws_api_gateway_rest_api.site_core_api_gateway.root_resource_id}"
-  lambda_function_arn  = "${aws_lambda_function.articles.invoke_arn}"
-  lambda_function_name = "${aws_lambda_function.articles.function_name}"
-  authorizer_id        =  "${aws_api_gateway_authorizer.authorizer.id}"
-  request_validator_id = "${aws_api_gateway_request_validator.core_api_request_validator.id}"
-}
-
 module "topic_api" {
   source  = "./api/topic"
   
