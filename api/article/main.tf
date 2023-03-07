572a64bbf0b2e25c0982218d8962e02506790236
tqliu <tliu@handext.com>
2023-03-06 21:57:20 -0800

    Remove Article APIs

--- api/article/main.tf
+++ api/article/main.tf
@@ -1,8 +1,7 @@
 locals {
   resource_name           = "articles"
   leaf_resource_name      = "article_id"
-  level_one_http_methods  = ["GET", "POST"]
-  level_two_http_methods  = ["GET"]
+  level_one_http_methods  = ["POST"]
 }
 
 resource "aws_lambda_permission" "articles" {
@@ -17,28 +16,10 @@ resource "aws_lambda_permission" "articles" {
   source_arn = "${var.api_execution_arn}/*/${each.key}/${local.resource_name}"
 }
 
-resource "aws_lambda_permission" "article" {
-  for_each = toset( local.level_two_http_methods )
-
-  action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_function_name}"
-  principal     = "apigateway.amazonaws.com"
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${var.api_execution_arn}/*/${each.key}/${local.resource_name}/{${local.leaf_resource_name}}"
-}
-
 resource "aws_api_gateway_resource" "articles" {
   rest_api_id = var.api_id
   parent_id   = var.root_resource_id
   path_part   = local.resource_name
-}
-
-resource "aws_api_gateway_resource" "article" {
-  rest_api_id = var.api_id
-  parent_id   = aws_api_gateway_resource.articles.id
-  path_part   = "{${local.leaf_resource_name}}"
 }
 
 module "api-gateway-enable-cors-articles" {
@@ -48,18 +29,9 @@ module "api-gateway-enable-cors-articles" {
   api_resource_id = aws_api_gateway_resource.articles.id
 }
 
-module "api-gateway-enable-cors-article" {
-  source  = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-  api_id          = var.api_id
-  api_resource_id = aws_api_gateway_resource.article.id
-}
-
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [
-    module.articles_get_api,
-    module.articles_post_api,
-    module.article_get_api
+    module.articles_post_api
   ]
 
   rest_api_id = var.api_id
